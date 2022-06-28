1729644a64cdca7110397b00567d496b9a1653ff
hahnah <superhahnah@gmail.com>
2022-06-28 23:22:07 +0900

    /accordion/stitches ページを一旦作成 (/accordion/primitive のコピー)

---
+++ pages/accordion/stitches.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import type { NextPage } from 'next'
+import * as AccordionPrimitive from '@radix-ui/react-accordion'
+
+const AccordionDemoWithStitches: NextPage = () => {
+    return (
+        <AccordionPrimitive.Root type="single" defaultValue="item-1" collapsible>
+            <AccordionPrimitive.Item value="item-1">
+                <AccordionPrimitive.Trigger>Is it accessible?</AccordionPrimitive.Trigger>
+                <AccordionPrimitive.Content>Yes. It adheres to the WAI-ARIA design pattern.</AccordionPrimitive.Content>
+            </AccordionPrimitive.Item>
+
+            <AccordionPrimitive.Item value="item-2">
+                <AccordionPrimitive.Trigger>Is it unstyled?</AccordionPrimitive.Trigger>
+                <AccordionPrimitive.Content>
+                    Yes. It's unstyled by default, giving you freedom over the look and feel.
+                </AccordionPrimitive.Content>
+            </AccordionPrimitive.Item>
+
+            <AccordionPrimitive.Item value="item-3">
+                <AccordionPrimitive.Trigger>Can it be animated?</AccordionPrimitive.Trigger>
+                <AccordionPrimitive.Content>
+                    Yes! You can animate the Accordion with CSS or JavaScript.
+                </AccordionPrimitive.Content>
+            </AccordionPrimitive.Item>
+        </AccordionPrimitive.Root>
+    )
+}
+export default AccordionDemoWithStitches
